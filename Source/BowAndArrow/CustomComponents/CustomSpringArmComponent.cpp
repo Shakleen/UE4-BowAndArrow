d29b0cfa7f6ba032ea95b80b84e1c99dab0f32c0
shakleenishfar <shakleenishfar@gmail.com>
2022-05-13 22:01:17 +0600

    Added camera transition to and back from aiming stance

--- Source/BowAndArrow/CustomComponents/CustomSpringArmComponent.cpp
+++ Source/BowAndArrow/CustomComponents/CustomSpringArmComponent.cpp
@@ -3,3 +3,64 @@
 
 #include "CustomSpringArmComponent.h"
 
+#include <TimerManager.h>
+#include <GameFramework/Actor.h>
+
+#include "../SparrowCharacter.h"
+
+#define IS_EQUAL(Value1, Value2) FMath::Abs(Value1 - Value2) < 1e-4
+
+void UCustomSpringArmComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ASparrowCharacter* OwningPlayer = Cast<ASparrowCharacter>(GetOwner());
+	if (!OwningPlayer) return;
+	OwningPlayer->OnAimStateChange.AddUObject(this, &UCustomSpringArmComponent::OnAimStanceChange);
+}
+
+void UCustomSpringArmComponent::OnAimStanceChange(bool bAiming)
+{
+	bIsAiming = bAiming;
+	StartPerspectiveTransition();
+}
+
+void UCustomSpringArmComponent::StartPerspectiveTransition()
+{
+	if (GetOwner()->GetWorldTimerManager().IsTimerActive(PerspectiveTransitionTimer))
+	{
+		return;
+	}
+
+	GetOwner()->GetWorldTimerManager().SetTimer(
+		PerspectiveTransitionTimer,
+		this,
+		&UCustomSpringArmComponent::Transition,
+		0.01f,
+		true
+	);
+}
+
+void UCustomSpringArmComponent::Transition()
+{
+	FSpringArmConfig OutcomeAimMode = bIsAiming ? AimPerspective : ThirdPersonPerspective;
+
+	if (IS_EQUAL(TargetArmLength, OutcomeAimMode.TargetArmLength))
+	{
+		StopPerspectiveTransition();
+		return;
+	}
+
+	float Alpha = AimTransitionSpeed * GetWorld()->GetDeltaSeconds();
+
+	SocketOffset = FMath::Lerp(SocketOffset, OutcomeAimMode.SocketOffset, Alpha);
+	TargetArmLength = FMath::Lerp(TargetArmLength, OutcomeAimMode.TargetArmLength, Alpha);
+}
+
+void UCustomSpringArmComponent::StopPerspectiveTransition()
+{
+	if (GetOwner()->GetWorldTimerManager().IsTimerActive(PerspectiveTransitionTimer))
+	{
+		GetOwner()->GetWorldTimerManager().ClearTimer(PerspectiveTransitionTimer);
+	}
+}
--- Source/BowAndArrow/CustomComponents/CustomSpringArmComponent.h
+++ Source/BowAndArrow/CustomComponents/CustomSpringArmComponent.h
@@ -6,12 +6,46 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "CustomSpringArmComponent.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FSpringArmConfig
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+		float TargetArmLength;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+		FVector SocketOffset;
+
+	FSpringArmConfig() : TargetArmLength(300.f), SocketOffset(FVector(0.f)) {	}
+};
+
 UCLASS()
 class BOWANDARROW_API UCustomSpringArmComponent : public USpringArmComponent
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void OnAimStanceChange(bool bAiming);
+	void StartPerspectiveTransition();
+	void Transition();
+	void StopPerspectiveTransition();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration", meta = (AllowPrivateAccess = "true"))
+		FSpringArmConfig ThirdPersonPerspective;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration", meta = (AllowPrivateAccess = "true"))
+		FSpringArmConfig AimPerspective;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration", meta = (AllowPrivateAccess = "true", ClampMin = "0.1"))
+		float AimTransitionUpdateFrequency = 0.1f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration", meta = (AllowPrivateAccess = "true", ClampMin = "1"))
+		float AimTransitionSpeed = 10.f;
+
+	UPROPERTY()
+		FTimerHandle PerspectiveTransitionTimer;
+	UPROPERTY()
+		bool bIsAiming = false;
 	
 };
