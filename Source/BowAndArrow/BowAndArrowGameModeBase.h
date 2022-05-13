7f168b977626728a98861f9e88c7e8d0187e0f6d
shakleenishfar <shakleenishfar@gmail.com>
2022-05-13 20:09:24 +0600

    Add project files.

---
+++ Source/BowAndArrow/BowAndArrowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BowAndArrowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWANDARROW_API ABowAndArrowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
